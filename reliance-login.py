0cd71eec7d49da9b7ccbbdf97c98abba1f0df243
kanwaldiku <kanwal.diku@gmail.com>
2014-07-24 01:05:49 -0700

    Update reliance-login.py

--- reliance-login.py
+++ reliance-login.py
@@ -62,7 +62,7 @@ def internet_connect():
   '''try to connect to the internet'''
   code, headers, html, cur_opener = get_url("http://10.239.89.15/reliance/startportal_isg.do", timeout=10)
   if debug: print html
-  login_data = urllib.urlencode({'userId' : username, 'password' : password, 'action' : 'doLoginSubmit'})
+  login_data = urllib.urlencode({'userId' : 343331695693 , 'password' : 12345678, 'action' : 'doLoginSubmit'})
   code, headers, html, cur_opener = get_url('http://10.239.89.15/reliance/login.do', data=login_data, opener=cur_opener)
   if debug: print html
 
